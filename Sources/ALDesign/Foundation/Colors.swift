fc1099c4cfc59adaadadf95547ee5e79f962408a
SmisLee <smis@delightroom.com>
2021-08-11 11:58:41 +0900

    Colors public 키워드 추가

--- Sources/ALDesign/Foundation/Colors.swift
+++ Sources/ALDesign/Foundation/Colors.swift
@@ -12,7 +12,7 @@ extension UIColor {
     public struct ALColor {
         /// dark: #0E1117
         /// light: #F5F9FF
-        static var background: UIColor {
+        public static var background: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -27,7 +27,7 @@ extension UIColor {
         
         /// dark: #1A203B
         /// light: #E8F1FF
-        static var appbar: UIColor {
+        public static var appbar: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -42,7 +42,7 @@ extension UIColor {
         
         /// dark: #0E1117, 0.8
         /// light: #30435F, 0.8
-        static var scrim: UIColor {
+        public static var scrim: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -57,7 +57,7 @@ extension UIColor {
         
         /// dark: #30435F
         /// light: #BDCFED
-        static var onSurfaceDisabled: UIColor {
+        public static var onSurfaceDisabled: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -72,7 +72,7 @@ extension UIColor {
         
         /// dark: #171D2E
         /// light: #FFFFFF
-        static var surface: UIColor {
+        public static var surface: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -87,7 +87,7 @@ extension UIColor {
         
         /// dark: #F5F9FF
         /// light: #1F252E
-        static var onSurfaceHighEmphasis: UIColor {
+        public static var onSurfaceHighEmphasis: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -102,7 +102,7 @@ extension UIColor {
         
         /// dark: #F5F9FF
         /// light: #7689A5
-        static var onSurfaceMediumEmphasis: UIColor {
+        public static var onSurfaceMediumEmphasis: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -117,7 +117,7 @@ extension UIColor {
         
         /// dark: #4E617D
         /// light: #A9BBD9
-        static var onSurfaceDefault: UIColor {
+        public static var onSurfaceDefault: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -132,7 +132,7 @@ extension UIColor {
         
         /// dark: #30435F
         /// light: #D2E4FF
-        static var onSurfaceDivider12: UIColor {
+        public static var onSurfaceDivider12: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -147,7 +147,7 @@ extension UIColor {
         
         /// dark: #30435F, 0.5
         /// light: #E8F1FF, 0.5
-        static var onSurfaceDivider5: UIColor {
+        public static var onSurfaceDivider5: UIColor {
             let result: UIColor
             if #available(iOS 13.0, *) {
                 switch UITraitCollection.current.userInterfaceStyle {
@@ -161,78 +161,78 @@ extension UIColor {
         }
         
         /// #F50F3B
-        static var primary: UIColor {
+        public static var primary: UIColor {
             return UIColor(hexString: "#F50F3B")
         }
         
         /// #C10020
-        static var primaryVariant: UIColor {
+        public static var primaryVariant: UIColor {
             return UIColor(hexString: "#C10020")
         }
         
         /// #FFFFFF
-        static var onPrimary: UIColor {
+        public static var onPrimary: UIColor {
             return UIColor(hexString: "#FFFFFF")
         }
         
         // TODO: gradient 방향 고려 필요
         /// #F50026, #FA005C, #FF9800
-        static var premiumCTA: [UIColor] {
+        public static var premiumCTA: [UIColor] {
             return [UIColor(hexString: "#F50026"), UIColor(hexString: "#FA005C"), UIColor(hexString: "#FF9800")]
         }
         
         /// #FA005C, #FF9800
-        static var premiumHorizon: [UIColor] {
+        public static var premiumHorizon: [UIColor] {
             return [UIColor(hexString: "#FA005C"), UIColor(hexString: "#FF9800")]
         }
         
         /// #FA005C, #FF9800
-        static var premiumVertical: [UIColor] {
+        public static var premiumVertical: [UIColor] {
             return [UIColor(hexString: "#FA005C"), UIColor(hexString: "#FF9800")]
         }
         
         /// #00A3C7
-        static var secondary: UIColor {
+        public static var secondary: UIColor {
             return UIColor(hexString: "#00A3C7")
         }
         
         /// #009299
-        static var secondaryVariant: UIColor {
+        public static var secondaryVariant: UIColor {
             return UIColor(hexString: "#009299")
         }
         
         /// #FEFEFE
-        static var onSecondary: UIColor {
+        public static var onSecondary: UIColor {
             return UIColor(hexString: "#FEFEFE")
         }
         
         /// #FFDA15
-        static var color1: UIColor {
+        public static var color1: UIColor {
             return UIColor(hexString: "#FFDA15")
         }
         
         /// #FF9800
-        static var color2: UIColor {
+        public static var color2: UIColor {
             return UIColor(hexString: "#FF9800")
         }
         
         /// #FA005C
-        static var color3: UIColor {
+        public static var color3: UIColor {
             return UIColor(hexString: "#FA005C")
         }
         
         /// #FF5D00
-        static var negative: UIColor {
+        public static var negative: UIColor {
             return UIColor(hexString: "#FF5D00")
         }
         
         /// #22BB3C
-        static var positive: UIColor {
+        public static var positive: UIColor {
             return UIColor(hexString: "#22BB3C")
         }
         
         /// #E2B00A
-        static var caution: UIColor {
+        public static var caution: UIColor {
             return UIColor(hexString: "#E2B00A")
         }
     }
