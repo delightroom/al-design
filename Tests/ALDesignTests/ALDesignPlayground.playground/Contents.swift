c94e4ac313cbb855e921be914daea3ce075e9b84
SmisLee <smis@delightroom.com>
2021-08-11 14:56:35 +0900

    CommonIcon 추가

--- Tests/ALDesignTests/ALDesignPlayground.playground/Contents.swift
+++ Tests/ALDesignTests/ALDesignPlayground.playground/Contents.swift
@@ -13,15 +13,21 @@ label.attributedText = "Hello ALDesign".hero.two.addColor(UIColor.onSurfaceMediu
 label.sizeToFit()
 
 let premiumImageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
-premiumImageView.image = PremiumIcon.premium.image
+premiumImageView.image = MissionPremiumIcon.premium.image
 premiumImageView.tintColor = .black
 premiumImageView.sizeToFit()
 
 let premiumGradientImageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
-premiumGradientImageView.image = PremiumIcon.premium.gradientImage
+premiumGradientImageView.image = MissionPremiumIcon.premium.gradientImage
 premiumGradientImageView.tintColor = .black
-premiumImageView.sizeToFit()
+premiumGradientImageView.sizeToFit()
 
 let mission16x16ImageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 16, height: 16)))
-mission16x16ImageView.image = PremiumIcon.missionMath.image
+mission16x16ImageView.image = MissionPremiumIcon.missionMath.image
 mission16x16ImageView.tintColor = .black
+
+
+let moreVerticalImageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
+moreVerticalImageView.image = CommonIcon.moreVertical.image
+moreVerticalImageView.tintColor = .black
+moreVerticalImageView.sizeToFit()
