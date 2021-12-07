e3aea1febc1f4398cdc2888f8f3748e22cdb7d9b
SmisLee <smis@delightroom.com>
2021-12-07 11:58:25 +0900

    Number.hero1 테스트 통과

--- Tests/ALDesignTests/TypgraphyTests.swift
+++ Tests/ALDesignTests/TypgraphyTests.swift
@@ -22,22 +22,41 @@ final class TypographyTests: XCTestCase {
             XCTFail()
         }
     }
-//    func testNumberHeroOne() {
-//        let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-//        let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
-//
-//        let input = "123456789"
-//        let output = input.number(.hero1)
-//        let correctOutput = NSAttributedString(string: input, attributes: attributes)
-//
-//        XCTAssertEqual(output, correctOutput)
-//    }
+    
+    func testNumberHeroOne() {
+        let fontToRegister = [
+              FontNameExt(name: "Lexend-SemiBold", ext: "ttf"),
+            ]
+        do {
+            try autoRegisteringFont(fontToRegister, "ALDesign")
+        } catch {
+            dump(error)
+            XCTFail()
+        }
+        let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
+
+        let input = "123456789"
+        let output = input.number(.hero1)
+        let correctOutput = NSAttributedString(string: input, attributes: attributes)
+
+        XCTAssertEqual(output, correctOutput)
+    }
 }
 
 extension String {
-//    func number(_ typo: Typography) -> NSAttributedString {
-//        let result: NSAttributedString
-//
-//        return result
-//    }
+    func number(_ typo: Typography) -> NSAttributedString {
+        let result: NSAttributedString
+        let font: UIFont = UIFont.lexendSemiBold(ofSize: 110)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
+        result = NSAttributedString(string: self, attributes: attributes)
+        return result
+    }
+}
+
+extension UIFont {
+    static func lexendSemiBold(ofSize size: CGFloat) -> UIFont {
+        guard let result = UIFont(name: "Lexend-SemiBold", size: size) else { return UIFont.systemFont(ofSize: size, weight: .heavy) }
+        return result
+    }
 }
