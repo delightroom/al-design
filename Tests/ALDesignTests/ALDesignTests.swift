0bc12d89c90ff6ca82b305ca16b5edac6d5d6bc5
SmisLee <smis@delightroom.com>
2021-12-08 09:19:29 +0900

    deprecated된 테스트 코드 제거

--- Tests/ALDesignTests/ALDesignTests.swift
+++ Tests/ALDesignTests/ALDesignTests.swift
@@ -2,65 +2,6 @@
     @testable import ALDesign
 
     final class ALDesignTests: XCTestCase {
-        func testTypography() {
-            let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero, testString)
-        }
-        
-        func testTypographyTwo() {
-            let font = UIFont.systemFont(ofSize: 48, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero.two, testString)
-        }
-        
-        func testTypographyThree() {
-            let font = UIFont.systemFont(ofSize: 12)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".caption.three, testString)
-        }
-        
-        func testTypographyAddColor() {
-            let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero.addColor(UIColor.onSurfaceHighEmphasis), testString)
-        }
-        
-        func testTypographyAddAlignment() {
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .left
-            let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis, .paragraphStyle: paragraphStyle]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero.addAlignment(.left), testString)
-        }
-        
-        func testTypographyAddColorAndAlignment() {
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .left
-            let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.onSurfaceHighEmphasis, .paragraphStyle: paragraphStyle]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero.addColor(UIColor.onSurfaceHighEmphasis).addAlignment(.left), testString)
-        }
-        
-        func testTypographyAddUIColor() {
-            let font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor(hexString: "#123456")]
-            let testString = NSAttributedString(string: "Test", attributes: attributes)
-            
-            XCTAssertEqual("Test".hero.addColor(UIColor(hexString: "#123456")), testString)
-        }
         
         func testColors() {
             let testColor = UIColor(hexString: "#F5F9FF")
