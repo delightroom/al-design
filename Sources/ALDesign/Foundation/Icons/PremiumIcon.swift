63ca0f081ab0f3c744b1826d94122e9d8fafd2fe
SmisLee <smis@delightroom.com>
2022-01-11 11:46:49 +0900

    PremiumIcon 추가

---
+++ Sources/ALDesign/Foundation/Icons/PremiumIcon.swift
@@ -0,0 +1,36 @@
+//
+//  PremiumIcon.swift
+//  
+//
+//  Created by smis on 2022/01/11.
+//
+
+import UIKit
+
+public struct PremiumIcon {
+    public static let premiumGradient2448 = UIImage(named: "ic_premium_gradient_24_48", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let timepressureGradient2424 = UIImage(named: "ic_timepressure_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let backupsoundGradient2424 = UIImage(named: "ic_backupsound_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let wucGradient2424 = UIImage(named: "ic_wuc_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let sppGradient2424 = UIImage(named: "ic_spp_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let labelReminderGradient2424 = UIImage(named: "ic_label_reminder_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseNewfeaturesGradient2424 = UIImage(named: "ic_purchase_newfeatures_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseTurnoffadsGradient2424 = UIImage(named: "ic_purchase_turnoffads_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let premiumGradient2424 = UIImage(named: "ic_premium_gradient_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let timepressure2424 = UIImage(named: "ic_timepressure_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let backupsound2424 = UIImage(named: "ic_backupsound_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let wuc2424 = UIImage(named: "ic_wuc_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let spp2424 = UIImage(named: "ic_spp_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let labelReminder2424 = UIImage(named: "ic_label_reminder_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseNewfeatures2424 = UIImage(named: "ic_purchase_newfeatures_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseTurnoffads2424 = UIImage(named: "ic_purchase_turnoffads_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let premium2424 = UIImage(named: "ic_premium_24_24", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let timepressure1616 = UIImage(named: "ic_timepressure_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let backupsound1616 = UIImage(named: "ic_backupsound_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let wuc1616 = UIImage(named: "ic_wuc_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let spp1616 = UIImage(named: "ic_spp_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let labelReminder1616 = UIImage(named: "ic_label_reminder_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseNewfeatures1616 = UIImage(named: "ic_purchase_newfeatures_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let purchaseTurnoffads1616 = UIImage(named: "ic_purchase_turnoffads_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+    public static let premium1616 = UIImage(named: "ic_premium_16_16", in: .module, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+}
