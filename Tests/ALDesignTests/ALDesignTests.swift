e783c2b94cdad82c819c0ef81bf25dc69335f6df
SmisLee <smis@delightroom.com>
2022-01-11 09:29:59 +0900

    NaviIcon 추가

--- Tests/ALDesignTests/ALDesignTests.swift
+++ Tests/ALDesignTests/ALDesignTests.swift
@@ -7,10 +7,4 @@
             let testColor = UIColor(hexString: "#F5F9FF")
             XCTAssertEqual(UIColor.background, testColor)
         }
-        
-        func testIcons() {
-            let sleepFilled = NaviIcon.sleep.filled
-            XCTAssertNotNil(sleepFilled)
-            XCTAssertEqual(CGSize(width: 24, height: 24), sleepFilled.size)   
-        }
     }
