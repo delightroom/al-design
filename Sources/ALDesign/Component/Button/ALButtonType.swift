70ed1645a54c3811fa7bfa10c97275db0592530b
SmisLee <smis@delightroom.com>
2022-01-14 11:18:21 +0900

    y값 수정

--- Sources/ALDesign/Component/Button/ALButtonType.swift
+++ Sources/ALDesign/Component/Button/ALButtonType.swift
@@ -27,7 +27,7 @@ extension ALButtonSize {
                 result = NSMutableAttributedString()
                 let attatchment = NSTextAttachment()
                 attatchment.image = SolidIcon.play1616
-                attatchment.bounds = CGRect(origin: CGPoint(x: 0, y: 1), size: SolidIcon.play1616.size)
+                attatchment.bounds = CGRect(origin: CGPoint(x: 0, y: -1), size: SolidIcon.play1616.size)
                 result.append(NSAttributedString(attachment: attatchment))
                 result.append(" \(title)".text(.overline))
             case .lineIconRight:
