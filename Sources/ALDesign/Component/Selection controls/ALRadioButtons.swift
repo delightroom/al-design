de8b547b8fd11d44cdf94d59705daca01ae773ab
SmisLee <smis@delightroom.com>
2022-02-03 14:21:37 +0900

    ALRadioButtons 수정

--- Sources/ALDesign/Component/Selection controls/ALRadioButtons.swift
+++ Sources/ALDesign/Component/Selection controls/ALRadioButtons.swift
@@ -10,15 +10,17 @@ import UIKit
 import Combine
 
 public class ALRadioButtons: UITableView {
-    
     private let labels: [String]
+    private var selectedIndex: Int?
     
     public init(frame: CGRect = .zero, labels: [String]) {
         self.labels = labels
         super.init(frame: frame, style: .plain)
+        translatesAutoresizingMaskIntoConstraints = false
         register(ALRadioButtonCell.self, forCellReuseIdentifier: "ALRadioButtonCell")
         dataSource = self
         delegate = self
+        separatorStyle = .none
     }
     
     required init?(coder: NSCoder) {
@@ -34,6 +36,11 @@ extension ALRadioButtons: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ALRadioButtonCell", for: indexPath) as? ALRadioButtonCell else { return UITableViewCell() }
         cell.update(for: labels[indexPath.row])
+        if indexPath.row == selectedIndex {
+            cell.select()
+        } else {
+            cell.deselect()
+        }
         return cell
     }
 }
@@ -44,31 +51,29 @@ extension ALRadioButtons: UITableViewDelegate {
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ALRadioButtonCell", for: indexPath) as? ALRadioButtonCell else { return }
-        cell.select()
-    }
-    
-    public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ALRadioButtonCell", for: indexPath) as? ALRadioButtonCell else { return }
-        cell.deselect()
+        selectedIndex = indexPath.row
+        reloadData()
     }
 }
 
 class ALRadioButtonCell: UITableViewCell {
     lazy private var selection: UIImageView = {
         let button = UIImageView()
-        button.backgroundColor = .black
-//        button.image = UIImage(named: "radio_button_unselected", in: .module, compatibleWith: nil)!
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.image = UIImage(named: "radio_button_unselected", in: .module, compatibleWith: nil)!
         return button
     }()
     
     lazy private var titleLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        
         addSubview(selection)
         selection.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         selection.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
@@ -77,6 +82,7 @@ class ALRadioButtonCell: UITableViewCell {
         selection.heightAnchor.constraint(equalToConstant: 54).isActive = true
         
         addSubview(titleLabel)
+        titleLabel.leadingAnchor.constraint(equalTo: selection.trailingAnchor).isActive = true
         titleLabel.centerYAnchor.constraint(equalTo: selection.centerYAnchor).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12).isActive = true
     }
@@ -90,10 +96,10 @@ class ALRadioButtonCell: UITableViewCell {
     }
     
     func select() {
-        selection.backgroundColor = .secondary
+        button.image = UIImage(named: "radio_button_selected", in: .module, compatibleWith: nil)!
     }
     
     func deselect() {
-        selection.backgroundColor = .black
+        button.image = UIImage(named: "radio_button_unselected", in: .module, compatibleWith: nil)!
     }
 }
