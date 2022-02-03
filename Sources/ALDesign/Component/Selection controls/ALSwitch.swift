29d99f7cd93b5d27e0205a67f2abf563618fe385
SmisLee <smis@delightroom.com>
2022-02-03 11:25:52 +0900

    ALSwitch 추가

---
+++ Sources/ALDesign/Component/Selection controls/ALSwitch.swift
@@ -0,0 +1,20 @@
+//
+//  ALSwitch.swift
+//  
+//
+//  Created by smis on 2022/02/03.
+//
+
+import Foundation
+import UIKit
+
+public class ALSwitch: UISwitch {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        onTintColor = .secondary
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
