4182f5554f94135322d99a6270243c16049d0237
smis <smis@delightroom.com>
2022-02-25 08:55:24 +0900

    sleepSound, todaypanel, more, history UITabBarItem 추가

--- Sources/ALDesign/Component/TabBarItem/ALTabBar.swift
+++ Sources/ALDesign/Component/TabBarItem/ALTabBar.swift
@@ -23,7 +23,44 @@ extension UITabBarItem {
         item.selectedImage = NaviIcon.alarmFilled2424.withTintColor(.surfaceHighEmphasis)
         item.tag = 401
         item.accessibilityLabel = MainTabUI.homeTabBarItem
-        homeTB.accessibilityIdentifier = MainTabUI.homeTabBarItem
+        item.accessibilityIdentifier = MainTabUI.homeTabBarItem
+        return item
+    }
+    
+    public static var sleepSound: UITabBarItem {
+        let item = UITabBarItem()
+        item.image = NaviIcon.sleepOutline2424.withTintColor(.surfaceLowEmphasis)
+        item.selectedImage = NaviIcon.settingFilled2424.withTintColor(.surfaceHighEmphasis)
+        item.tag = 402
+        item.accessibilityLabel = MainTabUI.sleepTabBarItem
+        item.accessibilityIdentifier = MainTabUI.sleepTabBarItem
+        return item
+    }
+    
+    public static var todaypanel: UITabBarItem {
+        let item = UITabBarItem()
+        item.image = NaviIcon.todaypanelOutline2424.withTintColor(.surfaceLowEmphasis)
+        item.selectedImage = NaviIcon.todaypanelFilled2424.withTintColor(.surfaceHighEmphasis)
+        item.tag = 403
+        item.accessibilityLabel = MainTabUI.todayTabBarItem
+        item.accessibilityIdentifier = MainTabUI.todayTabBarItem
+        return item
+    }
+    
+    public static var more: UITabBarItem {
+        let item = UITabBarItem()
+        item.image = NaviIcon.settingOutline2424.withTintColor(.surfaceLowEmphasis)
+        item.selectedImage = NaviIcon.settingFilled2424.withTintColor(.surfaceHighEmphasis)
+        item.tag = 404
+        item.accessibilityLabel = MainTabUI.moreTabBarItem
+        item.accessibilityIdentifier = MainTabUI.moreTabBarItem
+        return item
+    }
+    
+    public static var history: UITabBarItem {
+        let item = UITabBarItem()
+        item.image = NaviIcon.historyOutline2424.withTintColor(.surfaceLowEmphasis)
+        item.selectedImage = NaviIcon.historyFilled2424.withTintColor(.surfaceHighEmphasis)
         return item
     }
 }
