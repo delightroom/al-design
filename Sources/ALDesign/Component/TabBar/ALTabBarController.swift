6dc024ae4449de343628f89d7af4a2e70f6eb773
smis <smis@delightroom.com>
2022-02-25 09:33:02 +0900

    ALTabBarController 추가

---
+++ Sources/ALDesign/Component/TabBar/ALTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  ALTabBarController.swift
+//  
+//
+//  Created by 이옥민 on 2022/02/25.
+//
+
+import Foundation
+import UIKit
+
+class ALTabBarController: UITabBarController {
+    lazy private var indicatorImage: UIImage = {
+        let size = CGSize(width: 24, height: 35)
+        let rect = CGRect(origin: .zero, size: size)
+        let rectLine = CGRect(x:0, y: 32, width: 24,height: 2)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        UIColor.clear.setFill()
+        UIRectFill(rect)
+        UIColor.surfaceHighEmphasis.setFill()
+        UIRectFill(rectLine)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().selectionIndicatorImage = indicatorImage
+        tabBar.barTintColor = .surface
+        tabBar.isTranslucent = false
+    }
+}
