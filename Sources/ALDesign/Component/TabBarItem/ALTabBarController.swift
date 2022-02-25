eb4b6a5c06ee4a9b4c71b6b0274c9c6ea4b99ade
smis <smis@delightroom.com>
2022-02-25 09:53:01 +0900

    ALTabBarConroller에 badge 관련 로직 추가

--- Sources/ALDesign/Component/TabBarItem/ALTabBarController.swift
+++ Sources/ALDesign/Component/TabBarItem/ALTabBarController.swift
@@ -9,6 +9,19 @@ import Foundation
 import UIKit
 
 open class ALTabBarController: UITabBarController {
+    
+    public enum TabBarItem: Int {
+        case home
+        case sleepSound
+        case today
+        case more
+        case history
+        
+        var index: Int {
+            return self.rawValue
+        }
+    }
+    
     lazy private var indicatorImage: UIImage = {
         let size = CGSize(width: 24, height: 35)
         let rect = CGRect(origin: .zero, size: size)
@@ -28,5 +41,43 @@ open class ALTabBarController: UITabBarController {
         UITabBar.appearance().selectionIndicatorImage = indicatorImage
         tabBar.barTintColor = .surface
         tabBar.isTranslucent = false
+    }
+    
+    open func addCustomNewBadge(at item: TabBarItem) {
+        let barButtons = tabBar.subviews.filter { $0 is UIControl }
+        guard barButtons.count > item.index, let tabBarItemImage = tabBar.items?[item.index].image else { return }
+
+        let tabBarButton = barButtons[item.index]
+        let padding: CGFloat = 5
+        let x = tabBarButton.bounds.width/2 + (tabBarItemImage.size.width/2 + padding)
+        let y = tabBarButton.bounds.height/2 - (tabBarItemImage.size.width/2 + padding)
+        let size = CGSize(width: 4, height: 4)
+        let customBadge = CustomBadgeView(frame: CGRect(x: x, y: y, width: size.width, height: size.height))
+        
+        tabBarButton.subviews.forEach { ( $0 as? CustomBadgeView)?.removeFromSuperview() }
+        tabBarButton.addSubview(customBadge)
+    }
+    
+    open func hideCustomeNewBadge(at item: TabBarItem) {
+        let barButtons = tabBar.subviews.filter { $0 is UIControl }
+        guard barButtons.count > item.index else { return }
+        let tabBarButton = barButtons[item.index]
+        tabBarButton.subviews.forEach { ( $0 as? CustomBadgeView)?.removeFromSuperview() }
+    }
+}
+
+private class CustomBadgeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("This class does not support NSCoding")
+    }
+    
+    private func setup() {
+        self.backgroundColor = UIColor.primary
+        self.layer.cornerRadius = bounds.width/2
     }
 }
