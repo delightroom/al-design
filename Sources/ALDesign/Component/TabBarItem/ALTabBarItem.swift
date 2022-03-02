9e5929add1c3b64f83e14442f81d1a5eeaaaf399
smis <smis@delightroom.com>
2022-03-02 18:24:44 +0900

    alarm tabbaritem imageInsets 추가

--- Sources/ALDesign/Component/TabBarItem/ALTabBarItem.swift
+++ Sources/ALDesign/Component/TabBarItem/ALTabBarItem.swift
@@ -24,6 +24,7 @@ extension UITabBarItem {
         item.tag = 401
         item.accessibilityLabel = MainTabUI.homeTabBarItem
         item.accessibilityIdentifier = MainTabUI.homeTabBarItem
+        item.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         return item
     }
     
