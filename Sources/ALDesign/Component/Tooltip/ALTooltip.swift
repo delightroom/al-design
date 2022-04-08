5800a07b8c8861598bb2791f2a16bca38a52271a
smis <smis@delightroom.com>
2022-04-08 12:26:20 +0900

    ALIO-607 ALTooltip 추가

---
+++ Sources/ALDesign/Component/Tooltip/ALTooltip.swift
@@ -0,0 +1,89 @@
+//
+//  ALTooltip.swift
+//  
+//
+//  Created by 이옥민 on 2022/04/07.
+//
+
+import Foundation
+import UIKit
+
+public class TooltipArrowView: UIView {
+    override public func draw(_ rect: CGRect) {
+        super.draw(rect)
+        backgroundColor = .clear
+        print(frame)
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: frame.width / 2, y: frame.height))
+        path.addLine(to: CGPoint(x: frame.width, y: 0))
+        path.close()
+        
+        UIColor(hexString: "#00BEC7").set()
+        path.fill()
+    }
+}
+
+public class ALTooltip: UIView {
+    private let arrowDirection: ALTooltipArrowDirection
+    private let title: String
+    
+    lazy private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = title
+        return label
+    }()
+    
+    lazy private var messageLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    lazy private var arrowView: TooltipArrowView = {
+        let view = TooltipArrowView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    public init(title: String, arrowDirection: ALTooltipArrowDirection) {
+        self.title = title
+        self.arrowDirection = arrowDirection
+        super.init(frame: .zero)
+        initView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 6
+        backgroundColor = UIColor(hexString: "#00BEC7")
+        
+        addSubview(titleLabel)
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
+        
+        addTailView()
+    }
+    
+    private func addTailView() {
+        addSubview(arrowView)
+        switch arrowDirection {
+        case .top:
+            arrowView.bottomAnchor.constraint(equalTo: topAnchor).isActive = true
+            arrowView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        case .bottom:
+            arrowView.topAnchor.constraint(equalTo: bottomAnchor).isActive = true
+            arrowView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        }
+        arrowView.widthAnchor.constraint(equalToConstant: 7).isActive = true
+        arrowView.heightAnchor.constraint(equalToConstant: 8).isActive = true
+    }
+}
---
+++ Sources/ALDesign/Component/Tooltip/ALTooltipType.swift
@@ -0,0 +1,19 @@
+//
+//  ALTooltipType.swift
+//  
+//
+//  Created by 이옥민 on 2022/04/07.
+//
+
+import Foundation
+
+public enum ALTooltipType {
+    case small
+    case basic
+    case contents
+}
+
+public enum ALTooltipArrowDirection {
+    case top
+    case bottom
+}
