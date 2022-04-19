7aade4e6d6b62b5aec3047b645277a7942c133fb
smis <smis@delightroom.com>
2022-04-19 13:42:38 +0900

    ALIO-635 typography 수정

--- Sources/ALDesign/Foundation/Typography/Typography.swift
+++ Sources/ALDesign/Foundation/Typography/Typography.swift
@@ -55,49 +55,21 @@ extension String {
     }
     
     public func text(_ typo: Typography) -> NSMutableAttributedString {
-        let lineHeight: CGFloat
         let font: UIFont
         switch typo {
-        case .hero1:
-            font = UIFont.systemFont(ofSize: 110, weight: .heavy)
-            lineHeight = 110
-        case .hero2:
-            font = UIFont.systemFont(ofSize: 48, weight: .heavy)
-            lineHeight = 62
-        case .title1:
-            font = UIFont.systemFont(ofSize: 32, weight: .heavy)
-            lineHeight = 42
-        case .title2:
-            font = UIFont.systemFont(ofSize: 26, weight: .heavy)
-            lineHeight = 34
-        case .title3:
-            font = UIFont.systemFont(ofSize: 20, weight: .heavy)
-            lineHeight = 26
-        case .subtitle:
-            font = UIFont.systemFont(ofSize: 18)
-            lineHeight = 24
-        case .button:
-            font = UIFont.systemFont(ofSize: 18, weight: .heavy)
-            lineHeight = 24
-        case .paragraph:
-            font = UIFont.systemFont(ofSize: 16)
-            lineHeight = 22
-        case .overline:
-            font = UIFont.systemFont(ofSize: 14, weight: .heavy)
-            lineHeight = 20
-        case .caption1:
-            font = UIFont.systemFont(ofSize: 14)
-            lineHeight = 20
-        case .caption2:
-            font = UIFont.systemFont(ofSize: 12, weight: .heavy)
-            lineHeight = 18
-        case .caption3:
-            font = UIFont.systemFont(ofSize: 12)
-            lineHeight = 18
+        case .hero1: font = UIFont.systemFont(ofSize: 110, weight: .heavy)
+        case .hero2: font = UIFont.systemFont(ofSize: 48, weight: .heavy)
+        case .title1: font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        case .title2: font = UIFont.systemFont(ofSize: 26, weight: .heavy)
+        case .title3: font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+        case .subtitle: font = UIFont.systemFont(ofSize: 18)
+        case .button: font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        case .paragraph: font = UIFont.systemFont(ofSize: 16)
+        case .overline: font = UIFont.systemFont(ofSize: 14, weight: .heavy)
+        case .caption1: font = UIFont.systemFont(ofSize: 14)
+        case .caption2: font = UIFont.systemFont(ofSize: 12, weight: .heavy)
+        case .caption3: font = UIFont.systemFont(ofSize: 12)
         }
-        let style = NSMutableParagraphStyle()
-//        style.maximumLineHeight = lineHeight
-//        style.minimumLineHeight = lineHeight
         let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.surfaceHighEmphasis]
         return NSMutableAttributedString(string: self, attributes: attributes)
     }
