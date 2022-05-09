2d502f06a21b8a509e70d2649308f5f6238d0bc5
smis <smis@delightroom.com>
2022-05-09 16:23:43 +0900

    ALIO-656 backgroundColor 변경 추가

--- Sources/ALDesign/Component/NavigationBar/ALNavigationBar.swift
+++ Sources/ALDesign/Component/NavigationBar/ALNavigationBar.swift
@@ -8,16 +8,30 @@
 import UIKit
 
 public class ALNavigationBar: UINavigationBar {
+    private let textAttributes: [NSAttributedString.Key: Any] = [.font: Typography.title2.font, .foregroundColor: UIColor.onSurfaceHighEmphasis]
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         isTranslucent = false
         shadowImage = UIImage()
         prefersLargeTitles = true
-        largeTitleTextAttributes = [NSAttributedString.Key.font: Typography.title2.font]
+        largeTitleTextAttributes = textAttributes
         tintColor = .onSurfaceHighEmphasis
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override var backgroundColor: UIColor? {
+        didSet {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundColor = backgroundColor
+            appearance.titleTextAttributes = textAttributes
+            appearance.largeTitleTextAttributes = textAttributes
+            standardAppearance = appearance
+            compactAppearance = appearance
+            scrollEdgeAppearance = appearance
+        }
     }
 }
