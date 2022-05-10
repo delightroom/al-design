d86428e0122580afb4d56bc20c1ccc3683e9bd06
smis <smis@delightroom.com>
2022-05-10 12:22:35 +0900

    ALIO-656 backbutton 관련 처리 추가

--- Sources/ALDesign/Component/NavigationBar/ALNavigationController.swift
+++ Sources/ALDesign/Component/NavigationBar/ALNavigationController.swift
@@ -11,11 +11,19 @@ public class ALNavigationController: UINavigationController {
     
     public init() {
         super.init(navigationBarClass: ALNavigationBar.self, toolbarClass: nil)
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 14.0, *) {
+            navigationItem.backButtonDisplayMode = .minimal
+        } else {
+            navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
     }
 }
 
