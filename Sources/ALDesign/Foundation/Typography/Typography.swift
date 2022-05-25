b8d9a8a93c217be2a0f22dc380c15a96a7797b1b
SmisLee <72716390+SmisLee@users.noreply.github.com>
2022-05-25 09:33:18 +0900

    [ALIO-738] hero1 폰트 수정 (#9)
    * ALIO-738 hero1 수정
    
    * ALIO-738 리팩터링

--- Sources/ALDesign/Foundation/Typography/Typography.swift
+++ Sources/ALDesign/Foundation/Typography/Typography.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-internal func lexendSemiBold(ofSize size: CGFloat) -> UIFont {
-    let lexendFont = UIFont(name: "Lexend-SemiBold", size: size)
+enum LexendType: String {
+    case semiBold = "Lexend-SemiBold"
+    case regular = "Lexend-Regular"
+}
+
+func lexend(_ type: LexendType, ofSize size: CGFloat) -> UIFont {
+    let lexendFont = UIFont(name: type.rawValue, size: size)
     let systemFont = UIFont.systemFont(ofSize: size, weight: .heavy)
     guard lexendFont == nil else { return lexendFont! }
-    guard let fontUrl = Bundle.module.url(forResource: "Lexend-SemiBold", withExtension: "ttf"),
+    guard let fontUrl = Bundle.module.url(forResource: type.rawValue, withExtension: "ttf"),
           let fontDataProvider = CGDataProvider(url: fontUrl as CFURL),
           let font = CGFont(fontDataProvider) else {
               return systemFont
@@ -20,7 +25,7 @@ internal func lexendSemiBold(ofSize size: CGFloat) -> UIFont {
     guard CTFontManagerRegisterGraphicsFont(font, &error) else {
         return systemFont
     }
-    return UIFont(name: "Lexend-SemiBold", size: size)!
+    return UIFont(name: type.rawValue, size: size)!
 }
 
 public enum Typography {
@@ -40,16 +45,15 @@ public enum Typography {
 
 extension String {
     public func number(_ typo: Typography) -> NSMutableAttributedString {
-        let fontSize: CGFloat
+        let font: UIFont
         switch typo {
-        case .hero1: fontSize = 110
-        case .hero2: fontSize = 48
-        case .title1: fontSize = 32
-        case .title2: fontSize = 26
-        case .title3: fontSize = 20
-        default: fontSize = 20
+        case .hero1: font = lexend(.regular, ofSize: 110)
+        case .hero2: font = lexend(.semiBold, ofSize: 48)
+        case .title1: font = lexend(.semiBold, ofSize: 32)
+        case .title2: font = lexend(.semiBold, ofSize: 26)
+        case .title3: font = lexend(.semiBold, ofSize: 20)
+        default: font = lexend(.semiBold, ofSize: 20)
         }
-        let font: UIFont = lexendSemiBold(ofSize: fontSize)
         let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: UIColor.surfaceHighEmphasis]
         return NSMutableAttributedString(string: self, attributes: attributes)
     }
@@ -57,7 +61,7 @@ extension String {
     public func text(_ typo: Typography) -> NSMutableAttributedString {
         let font: UIFont
         switch typo {
-        case .hero1: font = UIFont.systemFont(ofSize: 110, weight: .heavy)
+        case .hero1: font = UIFont.systemFont(ofSize: 110)
         case .hero2: font = UIFont.systemFont(ofSize: 48, weight: .heavy)
         case .title1: font = UIFont.systemFont(ofSize: 32, weight: .heavy)
         case .title2: font = UIFont.systemFont(ofSize: 26, weight: .heavy)
--- Tests/ALDesignTests/TypgraphyTests.swift
+++ Tests/ALDesignTests/TypgraphyTests.swift
@@ -10,12 +10,12 @@ import XCTest
 
 final class TypographyTests: XCTestCase {
     func testlexend() {
-        let font = lexendSemiBold(ofSize: 11)
+        let font = lexend(.semiBold, ofSize: 11)
         XCTAssertNotNil(font)
     }
     
     func testNumberTypo() {
-        let fonts = [lexendSemiBold(ofSize: 110), lexendSemiBold(ofSize: 48), lexendSemiBold(ofSize:32), lexendSemiBold(ofSize: 26), lexendSemiBold(ofSize: 20)]
+        let fonts = [lexend(.regular, ofSize: 110), lexend(.semiBold, ofSize: 48), lexend(.semiBold, ofSize:32), lexend(.semiBold, ofSize: 26), lexend(.semiBold, ofSize: 20)]
 
         let input = "123456789"
         let output = [input.number(.hero1), input.number(.hero2), input.number(.title1), input.number(.title2), input.number(.title3)]
@@ -25,7 +25,7 @@ final class TypographyTests: XCTestCase {
     }
     
     func testTextTypo() {
-        let fonts: [UIFont] = [UIFont.systemFont(ofSize: 110, weight: .heavy), UIFont.systemFont(ofSize: 48, weight: .heavy), UIFont.systemFont(ofSize: 32, weight: .heavy), UIFont.systemFont(ofSize: 26, weight: .heavy), UIFont.systemFont(ofSize: 20, weight: .heavy), UIFont.systemFont(ofSize: 18, weight: .heavy), UIFont.systemFont(ofSize: 16), UIFont.systemFont(ofSize: 14, weight: .heavy), UIFont.systemFont(ofSize: 14), UIFont.systemFont(ofSize: 12, weight: .heavy), UIFont.systemFont(ofSize: 12)]
+        let fonts: [UIFont] = [UIFont.systemFont(ofSize: 110), UIFont.systemFont(ofSize: 48, weight: .heavy), UIFont.systemFont(ofSize: 32, weight: .heavy), UIFont.systemFont(ofSize: 26, weight: .heavy), UIFont.systemFont(ofSize: 20, weight: .heavy), UIFont.systemFont(ofSize: 18, weight: .heavy), UIFont.systemFont(ofSize: 16), UIFont.systemFont(ofSize: 14, weight: .heavy), UIFont.systemFont(ofSize: 14), UIFont.systemFont(ofSize: 12, weight: .heavy), UIFont.systemFont(ofSize: 12)]
 
         let input = "Good Morning"
         let output = [input.text(.hero1), input.text(.hero2), input.text(.title1), input.text(.title2), input.text(.title3), input.text(.subtitle1), input.text(.paragraph), input.text(.overline), input.text(.caption1), input.text(.caption2), input.text(.caption3)]
